045ace7a03960df5c0ba5712667c673815d2077d
Saiful Setiawan <saiful.setiawan@mitrais.com>
2022-03-09 14:46:08 +0700

    create sub microfrontend

---
+++ goodloking-site/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function App() {
+  return (
+    <div
+      style={{
+        background: "#dedece",
+        padding: "1rem 0.5rem",
+        margin: "0 1rem",
+      }}
+    >
+      <h2 style={{ color: "#1212ca" }}>This is a good looking site!</h2>
+      <div
+        style={{
+          padding: "1rem",
+          margin: "1rem 10px",
+          backgroundColor: "#fefefd",
+          borderRadius: "5px",
+        }}
+      >
+        <p>
+          ante in nibh mauris cursus mattis molestie a iaculis at erat
+          pellentesque adipiscing commodo elit at imperdiet dui accumsan sit
+          amet nulla facilisi morbi tempus iaculis urna id volutpat lacus
+          laoreet non curabitur gravida arcu ac tortor dignissim convallis
+          aenean et tortor at risus viverra adipiscing at in tellus integer
+          feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit
+          amet porttitor eget dolor morbi non arcu risus quis varius quam
+          quisque id diam vel
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
